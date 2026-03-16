8bb98a6f826a4034b451deea589a1343558fbd5f
Apple <apple@Apples-iMac.local>
2026-03-16 14:17:07 +0530

    Replace remaining gsap-trial import

--- src/types/gsap-trial.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "gsap-trial/SplitText" {
-  export const SplitText: any;
-  export default SplitText;
-}
-
-declare module "gsap-trial/ScrollSmoother" {
-  export const ScrollSmoother: any;
-  export default ScrollSmoother;
-}
