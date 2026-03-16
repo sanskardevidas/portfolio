e313fd2925507063c82e4e54fd4e7342de0da121
Sanskar Devidas Dherange <sanskardevidas@gmail.com>
2026-03-16 18:25:12 +0530

    Update portfolio preview image

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -8,9 +8,9 @@ const Landing = ({ children }: PropsWithChildren) => {
         <div className="landing-intro">
           <h2>Hello! I'm</h2>
           <h1>
-            D.
+            D. SANSKAR
             <br />
-            <span>SANSKAR</span>
+            <span>DEVIDAS</span>
           </h1>
         </div>
 
